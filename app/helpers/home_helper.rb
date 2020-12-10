7fb3dc31871f4ed48e17fc1d7d5ba8e4bb8f4ba7
gustavogmartins <gustavogmartinss@gmail.com>
2020-12-10 18:07:08 -0300

    Criação do front-end

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,11 @@
 module HomeHelper
    # Logs in the given user.
-   def log_in(user)
-    session[:user_id] = user.id
+   def log_in(author)
+    session[:author_id] = author.id
   end
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_author ||= Author.find_by(id: session[:author_id])
   end
-
 end
